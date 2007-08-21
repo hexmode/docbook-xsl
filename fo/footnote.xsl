hexmode/docbook-xsl

commit b358e8a52ed99e412e0bd761eb543b19fc49326c

Fixed bug #1669601.
git-svn-id: https://svn.code.sf.net/p/docbook/code/trunk/xsl@6773 6cfa91a5-b114-0410-aa58-ce7c4e7541e0

fo/footnote.xsl
@@ -90,7 +90,7 @@
         <!-- FIXME: list in @from is probably not complete -->
         <xsl:number level="any" 
                     from="chapter|appendix|preface|article|refentry|bibliography" 
-                    count="footnote[not(@label)][not(ancestor::table) and not(ancestor::informaltable)]|ulink[$ulink.footnotes != 0][node()][@url != .][not(ancestor::footnote)]" 
+                    count="footnote[not(@label)][not(ancestor::table) and not(ancestor::informaltable)]|ulink[$ulink.footnotes != 0][node()][@url != .][not(ancestor::footnote)][ulink.show != 0]" 
                     format="1"/>
       </xsl:variable>
       <xsl:choose>

commit 4a4ea4b6627460326b93f394846b73e8ffa168a1

Fixed typo (introduced by me in r6773). Closes bug #1778340.
git-svn-id: https://svn.code.sf.net/p/docbook/code/trunk/xsl@7265 6cfa91a5-b114-0410-aa58-ce7c4e7541e0

fo/footnote.xsl
@@ -90,7 +90,7 @@
         <!-- FIXME: list in @from is probably not complete -->
         <xsl:number level="any" 
                     from="chapter|appendix|preface|article|refentry|bibliography" 
-                    count="footnote[not(@label)][not(ancestor::table) and not(ancestor::informaltable)]|ulink[$ulink.footnotes != 0][node()][@url != .][not(ancestor::footnote)][ulink.show != 0]" 
+                    count="footnote[not(@label)][not(ancestor::table) and not(ancestor::informaltable)]|ulink[$ulink.footnotes != 0][node()][@url != .][not(ancestor::footnote)][$ulink.show != 0]" 
                     format="1"/>
       </xsl:variable>
       <xsl:choose>